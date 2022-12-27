61055db2fb193607a5b2563e6720564638c03b8f
Cole K <cole.krant@gmail.com>
2022-12-27 08:05:36 -0700

    commiting skeleton of community

--- init_data/create.sql
+++ init_data/create.sql
@@ -3,7 +3,7 @@ CREATE TABLE users(
     username VARCHAR(50) NOT NULL,
 	password CHAR(60) NOT NULL,
 	admin INT,
-	img VARCHAR(50),					-- profile picture
+	img VARCHAR(300),					-- profile picture
 	class VARCHAR(50),					-- freshman, sophomore, junior or senior
 	major VARCHAR(50),					-- major
 	committee VARCHAR(50),				-- chapter committee
