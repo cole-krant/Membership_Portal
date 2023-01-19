537c5c9da025a840974debc74d499554614af208
Cole K <cole.krant@gmail.com>
2023-01-19 16:13:21 -0700

    admin functionality: edit a specific user

--- server.js
+++ server.js
@@ -327,6 +327,64 @@ app.post("/update_profile/picture", upload.single('profile_img') ,(req, res) => 
         })
 
 });
+/* UPDATE PROFILE -------------------------------------------------------------------- */
+app.post("/update_profile/basic-admin", (req, res) => {
+
+    const edit_id = parseInt(req.body.edit_id);
+    const values = [req.body.class, req.body.major, req.body.committee, edit_id];
+
+    console.log("USER_ID = " + req.body.edit_id);
+    const query = `
+    UPDATE
+        users
+    SET
+        class = $1,
+        major = $2,
+        committee = $3
+    WHERE
+        user_id = $4;`;
+
+    db.none(query, values)
+        .then((update) => {
+
+            req.session.save();
+
+            console.log("\n\nSuccessful Update: \n", req.session.user);
+            res.redirect("/update_profile");
+        })
+        .catch((error) => {
+            console.log("\n\nERROR: ", error.message || error);
+        })
+
+});
+/* UPDATE PROFILE PICTURE -------------------------------------------------------------------- */
+app.post("/update_profile/picture-admin", upload.single('profile_img') ,(req, res) => {   /* UPLOAD PARAMETER ALLOWS FOR DATABASE UPLOAD */
+
+    const edit_id = parseInt(req.body.edit_id);
+    const values = [req.file.buffer.toString('base64'), edit_id];
+
+    console.log("USER_ID = " + req.body.edit_id);
+    const query = `
+    UPDATE
+        users
+    SET
+        imgHERE = $1
+    WHERE
+        user_id = $2;`;
+
+    db.none(query, values)
+        .then((update) => {
+
+            req.session.save();
+
+            console.log("\n\nSuccessful Profile Picture Update: \n", req.session.user);
+            res.redirect("/update_profile");
+        })
+        .catch((error) => {
+            console.log("\n\nERROR: ", error.message || error);
+        })
+
+});
 /* UPDATE USER DATABASE POST INTERVIEW SUBMISSION --------------------------------------------------------- */
 app.post("/update_users", (req, res) => {
 
